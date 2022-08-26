e04fd0e66d8e43ad7075590bc0406ad151083dff
carloalbertofilho <carlosalberto_filho@outlook.com>
2022-08-26 20:03:16 -0300

    Remoção da abstração User por problema com o JPA

--- src/main/java/br/com/dh/ClinicaOdontologica/entity/ClientUser.java
+++ src/main/java/br/com/dh/ClinicaOdontologica/entity/ClientUser.java
@@ -4,13 +4,38 @@ import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Entity
-public class ClientUser extends User 
+public class ClientUser 
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
     @Column(nullable = false)
     private String rg;
 
--- src/main/java/br/com/dh/ClinicaOdontologica/entity/DentistUser.java
+++ src/main/java/br/com/dh/ClinicaOdontologica/entity/DentistUser.java
@@ -2,13 +2,38 @@ package br.com.dh.ClinicaOdontologica.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Entity
-public class DentistUser extends User 
+public class DentistUser 
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
     @Column(nullable = false)
     private String registration;
 
