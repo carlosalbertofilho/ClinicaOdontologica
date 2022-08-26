0e44b6ee0654a4aa9ffa8c859a0902acc4f13ed5
carloalbertofilho <carlosalberto_filho@outlook.com>
2022-08-26 15:55:45 -0300

    Adicionado as Entidades User e DentistUser

---
+++ src/main/java/br/com/dh/ClinicaOdontologica/entity/DentistUser.java
@@ -0,0 +1,13 @@
+package br.com.dh.ClinicaOdontologica.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import lombok.Builder;
+
+@Builder
+@Entity
+public class DentistUser extends User {
+    @Column(name = "registration", nullable = false)
+    private String registration;
+}
---
+++ src/main/java/br/com/dh/ClinicaOdontologica/entity/User.java
@@ -0,0 +1,35 @@
+package br.com.dh.ClinicaOdontologica.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public abstract class User implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+
+    @Column(name = "login", nullable = false)
+    private String login;
+
+    @Column(name = "senha", nullable = false)
+    private String password;
+}
