3a61804154d0252b98f87da65936a91f78de667f
carloalbertofilho <carlosalberto_filho@outlook.com>
2022-08-26 22:44:58 -0300

    Criado Controle para a Entidade Dentista, adicionado a dependência ModelMapper

--- src/main/java/br/com/dh/ClinicaOdontologica/ClinicaOdontologicaApplication.java
+++ src/main/java/br/com/dh/ClinicaOdontologica/ClinicaOdontologicaApplication.java
@@ -1,10 +1,24 @@
  package br.com.dh.ClinicaOdontologica;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClinicaOdontologicaApplication {
+
+	/**
+	 * Sets whether a property should be skipped or not when the property value is null
+	 * @return ModelMapper set to skip Null Enable
+	 */
+	@Bean
+	public ModelMapper modelMapper()
+	{
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setSkipNullEnabled(true);
+		return modelMapper;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaOdontologicaApplication.class, args);
---
+++ src/main/java/br/com/dh/ClinicaOdontologica/controller/DentistController.java
@@ -0,0 +1,81 @@
+package br.com.dh.ClinicaOdontologica.controller;
+
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.com.dh.ClinicaOdontologica.entity.Dentist;
+import br.com.dh.ClinicaOdontologica.service.DentistService;
+
+@RestController
+@RequestMapping("/Dentista")
+public class DentistController 
+{
+    @Autowired
+    private DentistService dentistService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Dentist save(@RequestBody Dentist dentist)
+    {
+        return dentistService.save(dentist);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Dentist> listDentist()
+    {
+        return dentistService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Dentist findDentistById(@PathVariable("id") Long id)
+    {
+        return dentistService.findById(id)
+                .orElseThrow(() ->  
+                    new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteDentist(@PathVariable("id") Long id)
+    {
+        dentistService.findById(id)
+            .map(dentist -> {
+                dentistService.deleteById(id);
+                return Void.TYPE;
+            }).orElseThrow(() -> 
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Dentist not found")
+            );
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateDentist(@PathVariable("id") Long id,
+                              @RequestBody Dentist dentist)
+    {
+        dentistService.findById(id)
+            .map(foundOnBase -> {
+                modelMapper.map(dentist, foundOnBase);
+                return Void.TYPE;
+            }).orElseThrow(() -> 
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Dentist not found")
+        );
+    }
+}
