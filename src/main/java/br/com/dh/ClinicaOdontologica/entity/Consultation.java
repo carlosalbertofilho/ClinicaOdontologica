63ba004cbf2feff904c05ffb1e109da3254fb396
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-08-27 12:19:09 -0300

    Organização dos Importes

--- src/main/java/br/com/dh/ClinicaOdontologica/entity/Consultation.java
+++ src/main/java/br/com/dh/ClinicaOdontologica/entity/Consultation.java
@@ -3,7 +3,6 @@ package br.com.dh.ClinicaOdontologica.entity;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
