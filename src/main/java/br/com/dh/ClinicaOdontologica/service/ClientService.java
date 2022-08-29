06d7c50885684cf1a9d89457d7a0a1fe2ba54a4f
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-08-29 16:04:26 -0300

    Configurado Log no Application.yml e adicionado log.info no ClientService

--- src/main/java/br/com/dh/ClinicaOdontologica/service/ClientService.java
+++ src/main/java/br/com/dh/ClinicaOdontologica/service/ClientService.java
@@ -3,6 +3,8 @@ package br.com.dh.ClinicaOdontologica.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,20 +17,26 @@ public class ClientService
     @Autowired
     private ClientRepository clientRepository;
 
+    private static Logger log = LogManager.getLogger(ClientRepository.class.getName());
+
     public Client save(Client client)
     {
+        log.info("Creating User: %s".formatted(client.getLogin()));
         return clientRepository.save(client);
     }
     public List<Client> FindAll()
     {
+        log.info("Find All Clients");
         return clientRepository.findAll();
     }
     public Optional<Client> findById(Long id)
     {
+        log.info("Find Client by ID: %d".formatted(id));
         return clientRepository.findById(id);
     }
     public void deleteById(Long id)
     {
+        log.info("Delete Client by ID: %d".formatted(id));
         clientRepository.deleteById(id);
     }
 }
