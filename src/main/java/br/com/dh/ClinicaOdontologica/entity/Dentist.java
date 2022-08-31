2a71242d67c93d4f32f87b55ecd27e1af713cb29
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-08-31 16:21:08 -0300

    Adicionado validação para a Entidade Dentista

--- src/main/java/br/com/dh/ClinicaOdontologica/entity/Dentist.java
+++ src/main/java/br/com/dh/ClinicaOdontologica/entity/Dentist.java
@@ -8,9 +8,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,25 +35,33 @@ public class Dentist implements Serializable
     private Long id;
 
     @NotBlank(message = "Dentist name is mandatory to fill!")
+    @Size(min = 2, max = 50, message = "Dentist Name accepts only upto 50 character and minimun 2 character")
     @Column(nullable = false)
     private String name;
 
     @NotBlank(message = "Dentist last name is mandatory to fill")
+    @Size(min = 2, max = 50, message = "Dentist lastName accepts only upto 50 character and minimun 2 character")
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
     @Column(nullable = false)
+    @Email(message = "Email invalid!")
     private String login;
 
     @Column(nullable = false)
+    @Size(min = 6, max = 12, message = "Client password accepts only upto 12 character and minimun 6 character")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]$"
+            ,message = "Password accepts only at least one uppercase letter, one lowercase letter and one number and one special character")
     private String password;
 
+    @CPF
     @Column(nullable = false)
     private String registration;
 
     @Column(nullable = false)
     private Boolean isAdmin;
 
-    @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDate createdAt;
 }
