3bb5172f74a74be22e0f652dbb0a954687763355
carloalbertofilho <carlosalberto_filho@outlook.com>
2022-09-12 20:27:30 -0300

    tentativa mock para ClientController

--- src/test/java/br/com/dh/ClinicaOdontologica/ClientControllerTest.java
+++ src/test/java/br/com/dh/ClinicaOdontologica/ClientControllerTest.java
@@ -18,6 +18,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.dh.ClinicaOdontologica.repository.ClientRepository;
+import net.minidev.json.JSONObject;
 
 @ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
@@ -30,6 +31,7 @@ public class ClientControllerTest
   @Autowired
   private ObjectMapper objectMapper;
 
+
   @MockBean
   ClientRepository clientRepository;
 
@@ -37,21 +39,22 @@ public class ClientControllerTest
   @DisplayName("Testa salvar Cliente e testa lista de cliente")
   public void itShouldSaveClientAndListThen() throws Exception
   {
-    String requestJson = "[{"
-     + "'name': 'Drogo',"
-     + "'lastName': 'Cachorrosa',"
-     + "'login': 'drogo@teste.com',"
-     + "'password': 'Teste@1234',"
-     + "'rg': '11.222.333-6',"
-     + "'address': 'rua 10' }]";
+
+    JSONObject resquest = new JSONObject();
+    resquest.put("name", "Drogo");
+    resquest.put("lastName", "Cachorroso");
+    resquest.put("login", "drogo@teste.com");
+    resquest.put("password", "Teste@1234");
+    resquest.put("rg", "11.222.333-6");
+    resquest.put("address", "rua 10");
 
 
     mockMvc.perform(MockMvcRequestBuilders
       .post("/api/cliente")
-      .content(objectMapper.writeValueAsString(requestJson))
+      .content(objectMapper.writeValueAsString(resquest))
       .contentType(MediaType.APPLICATION_JSON)
       .accept(MediaType.APPLICATION_JSON))
         .andExpect(status().isCreated())
-        .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+        .andExpect(MockMvcResultMatchers.jsonPath("$.name").exists());
   }
 }
