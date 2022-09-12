3ef00954404453643c33353eafb702588517ed6a
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-09-12 18:09:42 -0300

    Mundança do Resquest para string

--- src/test/java/br/com/dh/ClinicaOdontologica/ClientControllerTest.java
+++ src/test/java/br/com/dh/ClinicaOdontologica/ClientControllerTest.java
@@ -5,11 +5,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,51 +17,38 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import br.com.dh.ClinicaOdontologica.controller.ClientController;
-import br.com.dh.ClinicaOdontologica.dto.ClientDTO;
-import br.com.dh.ClinicaOdontologica.service.ClientService;
+import br.com.dh.ClinicaOdontologica.repository.ClientRepository;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(ClientController.class)
+@AutoConfigureMockMvc
+@WebMvcTest(ClientControllerTest.class)
 public class ClientControllerTest
 {
-
-  @TestConfiguration
-  static class ClientControllerTestConfiguration
-  {
-    @Bean
-    public ClientService clientService(){
-      return new ClientService();
-    }
-  }
-
-  @Autowired
-  ClientService clientService;
-
-  @Mock
-  ClientDTO clientDTO;
-
   @Autowired
   private MockMvc mockMvc;
 
   @Autowired
   private ObjectMapper objectMapper;
 
+  @MockBean
+  ClientRepository clientRepository;
+
   @Test
   @DisplayName("Testa salvar Cliente e testa lista de cliente")
   public void itShouldSaveClientAndListThen() throws Exception
   {
-    clientDTO.setName("Carlos");
-    clientDTO.setLastName("Alberto");
-    clientDTO.setLogin("carlos.filho@teste.com");
-    clientDTO.setPassword("123456");
-    clientDTO.setRg("12.123.123-22");
-    clientDTO.setAddress("Rua 01");
+    String requestJson = "[{"
+     + "'name': 'Drogo',"
+     + "'lastName': 'Cachorrosa',"
+     + "'login': 'drogo@teste.com',"
+     + "'password': 'Teste@1234',"
+     + "'rg': '11.222.333-6',"
+     + "'address': 'rua 10' }]";
 
 
     mockMvc.perform(MockMvcRequestBuilders
       .post("/api/cliente")
-      .content(objectMapper.writeValueAsString(clientDTO))
+      .content(objectMapper.writeValueAsString(requestJson))
       .contentType(MediaType.APPLICATION_JSON)
       .accept(MediaType.APPLICATION_JSON))
         .andExpect(status().isCreated())
