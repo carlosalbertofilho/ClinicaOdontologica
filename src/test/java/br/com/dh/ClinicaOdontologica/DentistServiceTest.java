5f84f5a0920feb22c3385fcb9483689b4910eac1
carbonedev <arboness@gmail.com>
2022-09-12 21:41:20 -0300

    class DentistServiceTest ok

---
+++ src/test/java/br/com/dh/ClinicaOdontologica/DentistServiceTest.java
@@ -0,0 +1,62 @@
+package br.com.dh.ClinicaOdontologica;
+
+import br.com.dh.ClinicaOdontologica.dto.DentistDTO;
+import br.com.dh.ClinicaOdontologica.entity.Client;
+import br.com.dh.ClinicaOdontologica.entity.Consultation;
+import br.com.dh.ClinicaOdontologica.entity.Dentist;
+import br.com.dh.ClinicaOdontologica.service.DentistService;
+import br.com.dh.ClinicaOdontologica.util.DentistUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@SpringBootTest
+public class DentistServiceTest {
+
+  @Autowired
+  DentistService service;
+
+  static Dentist dentist;
+
+  @BeforeAll
+  static void doBefore(){
+    dentist = new Dentist();
+    dentist.setRegistration("111111");
+    dentist.setName("Claudio");
+    dentist.setLastName("Duarte");
+    dentist.setLogin("claudio.duarte");
+    dentist.setPassword("cd1234");
+    dentist.setIsAdmin(true);
+  }
+
+  @Test
+  void successSave(){
+    DentistDTO dentistSave = service.save(DentistUtil.convertToDTO(dentist));
+    Assertions.assertNotNull(dentistSave.getId());
+
+  }
+
+  @Test
+  void findAllDentist(){
+    DentistDTO dentistSave = service.save(DentistUtil.convertToDTO(dentist));
+    List<DentistDTO> resultList = service.findAll();
+    Assertions.assertNotNull(resultList);
+
+  }
+
+  @Test
+  void findADentist(){
+    DentistDTO dentistSave = service.save(DentistUtil.convertToDTO(dentist));
+    Optional<Dentist> resultList = service.findById(1L);
+    Assertions.assertNotNull(resultList);
+
+  }
+
+
+}
