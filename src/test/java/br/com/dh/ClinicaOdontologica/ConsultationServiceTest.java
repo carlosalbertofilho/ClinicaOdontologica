122b3c2ed130e060901ee283f78274b893a1a4bb
carbonedev <arboness@gmail.com>
2022-09-12 22:10:00 -0300

    inicio do teste da classe ConsultationService

--- src/test/java/br/com/dh/ClinicaOdontologica/ConsultationServiceTest.java
+++ src/test/java/br/com/dh/ClinicaOdontologica/ConsultationServiceTest.java
@@ -1,11 +1,15 @@
 package br.com.dh.ClinicaOdontologica;
 
+import br.com.dh.ClinicaOdontologica.dto.ClientDTO;
 import br.com.dh.ClinicaOdontologica.dto.ConsultationDTO;
 import br.com.dh.ClinicaOdontologica.dto.DentistDTO;
 import br.com.dh.ClinicaOdontologica.entity.Client;
 import br.com.dh.ClinicaOdontologica.entity.Consultation;
 import br.com.dh.ClinicaOdontologica.entity.Dentist;
+import br.com.dh.ClinicaOdontologica.service.ClientService;
 import br.com.dh.ClinicaOdontologica.service.ConsultationService;
+import br.com.dh.ClinicaOdontologica.service.DentistService;
+import br.com.dh.ClinicaOdontologica.util.ClientUtil;
 import br.com.dh.ClinicaOdontologica.util.ConsultationUtil;
 import br.com.dh.ClinicaOdontologica.util.DentistUtil;
 import org.junit.jupiter.api.Assertions;
@@ -14,6 +18,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,15 +31,44 @@ public class ConsultationServiceTest {
   @Autowired
   ConsultationService service;
 
+
+
+
   static Consultation consultation;
+  static Client client;
+  static Dentist dentist;
 
   @BeforeAll
-  void doBefore(){
+  static void doBefore(){
     consultation = new Consultation();
-    consultation.getClient();
-    consultation.getDentist();
-    consultation.getScheduledDate();
-    consultation.getScheduledTime();
+    ClientService clientService = new ClientService();
+    client = new Client();
+    client.setRg("11111111-1");
+    client.setName("Antonio");
+    client.setLastName("Souza");
+    client.setLogin("antonio.souza");
+    client.setPassword("as1234");
+    client.setAddress("Rua vinte, 10");
+    client.setCreatedAt(LocalDate.now());
+    client.setUpdateAt(LocalDate.now());
+    clientService.save(ClientUtil.convertToDTO(client));
+
+    DentistService dentistService = new DentistService();
+    dentist = new Dentist();
+    dentist.setRegistration("111111");
+    dentist.setName("Claudio");
+    dentist.setLastName("Duarte");
+    dentist.setLogin("claudio.duarte");
+    dentist.setPassword("cd1234");
+    dentist.setIsAdmin(true);
+    dentistService.save(DentistUtil.convertToDTO(dentist));
+
+    consultation.setClient(client);
+    consultation.setDentist(dentist);
+    consultation.setScheduledDate(LocalDate.now());
+    consultation.setScheduledTime(Time.valueOf(LocalTime.now()));
+
+
   }
 
   @Test
