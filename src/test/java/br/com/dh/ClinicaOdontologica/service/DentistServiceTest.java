fe88b21870169693b498f6912911557e15ad28dd
carloalbertofilho <carlosalberto_filho@outlook.com>
2022-09-12 22:51:22 -0300

    Ajuste de importações

--- src/test/java/br/com/dh/ClinicaOdontologica/service/DentistServiceTest.java
+++ src/test/java/br/com/dh/ClinicaOdontologica/service/DentistServiceTest.java
@@ -1,19 +1,17 @@
 package br.com.dh.ClinicaOdontologica.service;
 
-import br.com.dh.ClinicaOdontologica.dto.DentistDTO;
-import br.com.dh.ClinicaOdontologica.entity.Client;
-import br.com.dh.ClinicaOdontologica.entity.Consultation;
-import br.com.dh.ClinicaOdontologica.entity.Dentist;
-import br.com.dh.ClinicaOdontologica.service.DentistService;
-import br.com.dh.ClinicaOdontologica.util.DentistUtil;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
-import java.util.Optional;
+import br.com.dh.ClinicaOdontologica.dto.DentistDTO;
+import br.com.dh.ClinicaOdontologica.entity.Dentist;
+import br.com.dh.ClinicaOdontologica.util.DentistUtil;
 
 
 @SpringBootTest
@@ -44,7 +42,6 @@ public class DentistServiceTest {
 
   @Test
   void findAllDentist(){
-    DentistDTO dentistSave = service.save(DentistUtil.convertToDTO(dentist));
     List<DentistDTO> resultList = service.findAll();
     Assertions.assertNotNull(resultList);
 
@@ -52,7 +49,6 @@ public class DentistServiceTest {
 
   @Test
   void findADentist(){
-    DentistDTO dentistSave = service.save(DentistUtil.convertToDTO(dentist));
     Optional<Dentist> resultList = service.findById(1L);
     Assertions.assertNotNull(resultList);
 
