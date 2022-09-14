c9fb488030120776c8dfaf6190682c9a3f011eda
Du-devBR <eduardo.ananiasdev@outlook.com>
2022-09-13 21:52:57 -0300

    adicionado crossorigin nos controllers

--- src/test/java/br/com/dh/ClinicaOdontologica/ClinicaOdontologicaApplicationTests.java
+++ src/test/java/br/com/dh/ClinicaOdontologica/ClinicaOdontologicaApplicationTests.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import br.com.dh.ClinicaOdontologica.controller.ClientController;
 
 
