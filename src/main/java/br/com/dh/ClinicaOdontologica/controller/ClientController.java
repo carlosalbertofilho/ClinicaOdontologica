31168bf924116cd86d74f59930f973d71ba60378
carloalbertofilho <carlosalberto_filho@outlook.com>
2022-09-15 20:32:07 -0300

    ajuste

--- src/main/java/br/com/dh/ClinicaOdontologica/controller/ClientController.java
+++ src/main/java/br/com/dh/ClinicaOdontologica/controller/ClientController.java
@@ -8,7 +8,6 @@ import javax.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +26,6 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@CrossOrigin("*")
 @RequestMapping("/api/cliente")
 public class ClientController
 {
@@ -77,8 +75,8 @@ public class ClientController
         clientService.deleteById(id);
         return Void.TYPE;
       }).orElseThrow(() ->
-      new ResponseStatusException(HttpStatus.NOT_FOUND
-        , "Client not found"));
+        new ResponseStatusException(HttpStatus.NOT_FOUND
+          ,"Client not found"));
   }
 
   @PutMapping("/{id}")
