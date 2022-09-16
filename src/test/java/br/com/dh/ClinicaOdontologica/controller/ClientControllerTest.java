f4c54047534e1750a7afb0f32de648e36d75c9e1
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-09-16 16:08:16 -0300

    Aplicando rebase

--- src/test/java/br/com/dh/ClinicaOdontologica/controller/ClientControllerTest.java
+++ src/test/java/br/com/dh/ClinicaOdontologica/controller/ClientControllerTest.java
@@ -1,48 +1,41 @@
 package br.com.dh.ClinicaOdontologica.controller;
 
 
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import br.com.dh.ClinicaOdontologica.dto.ClientDTO;
 import br.com.dh.ClinicaOdontologica.service.ClientService;
-import io.restassured.http.ContentType;
 
-
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(ClientController.class)
 public class ClientControllerTest
 {
-  @Autowired
-  private ClientController clientController;
-
   @MockBean
   private ClientService clientService;
 
-  @MockBean
-  private ConsultationController consultationController;
-
-  @BeforeEach
-  public void setup()
-  {
-    standaloneSetup(this.clientController);
-  }
+  @Autowired
+  MockMvc mockMvc;
 
   @Test
-  @DisplayName("Testa listar Clientes")
-  public void itShouldListClients()
+  @DisplayName("Teste de lista de Clientes")
+  public void itShouldListClients() throws Exception
   {
     List<ClientDTO> value = new ArrayList<>();
     value.add(new ClientDTO(Long.valueOf("1")
@@ -60,13 +53,31 @@ public class ClientControllerTest
       .thenReturn(value);
 
     // MAKE A TEST
-    given()
-      .accept(ContentType.JSON)
-    .when()
+    mockMvc.perform( MockMvcRequestBuilders
       .get("/api/cliente")
-    .then()
-      .statusCode(HttpStatus.OK.value());
+      .accept(MediaType.APPLICATION_JSON))
+      .andExpect( MockMvcResultMatchers.status().isOk() );
+  }
+
+  @Test
+  @DisplayName("Testa salvar Cliente")
+  public void itShouldSaveClientAndListThen() throws Exception
+  {
+    JSONObject resquest = new JSONObject();
+    resquest.put("name", "Drogo");
+    resquest.put("lastName", "Cachorroso");
+    resquest.put("login", "drogo@teste.com");
+    resquest.put("password", "Teste@1234");
+    resquest.put("rg", "11.222.333-6");
+    resquest.put("address", "rua 10");
+
+
+    mockMvc.perform(MockMvcRequestBuilders
+        .post("/api/cliente")
+        .content(resquest.toString())
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON))
+      .andExpect(MockMvcResultMatchers.status().isCreated());
   }
 
 }
-
