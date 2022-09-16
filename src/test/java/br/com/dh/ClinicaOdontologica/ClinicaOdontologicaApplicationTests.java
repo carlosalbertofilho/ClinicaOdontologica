bfd0929e380440ea7188081635d59afe71b56725
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-09-16 14:18:01 -0300

    Teste Controller Get FindAll em Client

--- src/test/java/br/com/dh/ClinicaOdontologica/ClinicaOdontologicaApplicationTests.java
+++ src/test/java/br/com/dh/ClinicaOdontologica/ClinicaOdontologicaApplicationTests.java
@@ -3,24 +3,33 @@ package br.com.dh.ClinicaOdontologica;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import br.com.dh.ClinicaOdontologica.controller.ClientController;
+import br.com.dh.ClinicaOdontologica.controller.ConsultationController;
+import br.com.dh.ClinicaOdontologica.controller.DentistController;
 
 
 @SpringBootTest
-@ExtendWith(SpringExtension.class)
-class ClinicaOdontologicaApplicationTests {
+class ClinicaOdontologicaApplicationTests
+{
 
   @Autowired
   ClientController clientController;
+
+  @Autowired
+  DentistController dentistController;
+
+  @Autowired
+  ConsultationController consultationController;
 
 	@Test
 	void contextLoads()
   {
     Assertions.assertThat(clientController).isNotNull();
+    Assertions.assertThat(dentistController).isNotNull();
+    Assertions.assertThat(consultationController).isNotNull();
 	}
 
 }
