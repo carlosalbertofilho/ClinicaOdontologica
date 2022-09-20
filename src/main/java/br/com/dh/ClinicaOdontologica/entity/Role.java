7289a5ae21d1f80533e0fa327f69d44fe3d86ceb
Du-devBR <eduardo.ananiasdev@outlook.com>
2022-09-20 16:37:47 -0300

    corrigido entity role

--- src/main/java/br/com/dh/ClinicaOdontologica/entity/Role.java
+++ src/main/java/br/com/dh/ClinicaOdontologica/entity/Role.java
@@ -1,11 +1,21 @@
 package br.com.dh.ClinicaOdontologica.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Role implements GrantedAuthority {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
