23403e710978f624dc01a2e7495cef9078314d91
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-09-22 11:48:39 -0300

    Remoção HQL do ClientRepository e DentistRepository

--- src/main/java/br/com/dh/ClinicaOdontologica/repository/DentistRepository.java
+++ src/main/java/br/com/dh/ClinicaOdontologica/repository/DentistRepository.java
@@ -3,12 +3,10 @@ package br.com.dh.ClinicaOdontologica.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import br.com.dh.ClinicaOdontologica.entity.Dentist;
 
 public interface DentistRepository extends JpaRepository<Dentist, Long>
 {
-//  @Query("FROM Dentist AS d WHERE d.login = 1")
   Optional<Dentist> findByLogin(String login);
 }
