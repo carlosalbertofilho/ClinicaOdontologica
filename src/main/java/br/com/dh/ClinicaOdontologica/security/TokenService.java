fbf6659b16abdf133bc6f177881d501c47ae14f5
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-09-24 11:32:32 -0300

    Ajuste de Imports

--- src/main/java/br/com/dh/ClinicaOdontologica/security/TokenService.java
+++ src/main/java/br/com/dh/ClinicaOdontologica/security/TokenService.java
@@ -1,6 +1,5 @@
 package br.com.dh.ClinicaOdontologica.security;
 
-import br.com.dh.ClinicaOdontologica.entity.UserAdmin;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -23,17 +22,16 @@ public class TokenService {
 
     Date dataHoje = new Date();
     Date dataExpiracao = new Date(dataHoje.getTime() + Long.parseLong(expiration));     //para definir a data de expiracao
-    String token =  Jwts.builder()
+
+    //applicatioProperties alterar
+
+    return Jwts.builder()
       .setIssuer("Api Dh Ecommerce")
       .setSubject(userLogged.getUsername())
       .setIssuedAt(dataHoje)
       .setExpiration(dataExpiracao)
       .signWith(SignatureAlgorithm.HS256,secret)
       .compact();
-
-    //applicatioProperties alterar
-
-    return token;
   }
 
   public boolean verifyToken(String token) {
@@ -47,7 +45,6 @@ public class TokenService {
 
   public  String getUserName(String token){
     Claims claims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody();
-    String username = claims.getSubject();
-    return username;
+    return claims.getSubject();
   }
 }
