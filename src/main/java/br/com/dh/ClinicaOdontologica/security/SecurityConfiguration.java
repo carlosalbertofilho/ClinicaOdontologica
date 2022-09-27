6101fb98039c50ffab4f9f005daf86002f617015
Du-devBR <eduardo.ananiasdev@outlook.com>
2022-09-27 20:37:00 -0300

    adionado documentação com swagger

--- src/main/java/br/com/dh/ClinicaOdontologica/security/SecurityConfiguration.java
+++ src/main/java/br/com/dh/ClinicaOdontologica/security/SecurityConfiguration.java
@@ -3,10 +3,12 @@ package br.com.dh.ClinicaOdontologica.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -36,7 +38,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable().authorizeRequests()
+    http.authorizeRequests()
       .antMatchers("/login").permitAll()
       // .antMatchers("/api/dentista").hasRole("ADMIN")
       // .antMatchers("/api/cliente").hasRole("ADMIN")
@@ -53,7 +55,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
       .antMatchers("/h2/**").permitAll()
       //.antMatchers("/api/consulta/{id}").hasAnyAuthority("ADMIN")
       .anyRequest().authenticated()
-      .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //adiciona para autenticar no front
+      .and().csrf().disable()
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //adiciona para autenticar no front
       .and().addFilterBefore(new TokenAutotication(tokenService, userAuThService), UsernamePasswordAuthenticationFilter.class);
 
     //.formLogin();
@@ -62,5 +65,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     //this will allow frames with same origin which is much more safe
     http.headers().frameOptions().sameOrigin();
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+      web.ignoring().antMatchers(
+              "/**.html",
+              "/v2/api-docs",
+              "/webjars/**",
+              "/configuration/**",
+              "/swagger-ui/**", "/v3/api-docs/**");
   }
 }
